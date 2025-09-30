b23010fc0a703f8bd5654063a1340f98dc9fe3ac
kolesnikovak <katyamiller36@gmail.com>
2025-09-29 21:02:07 -0500

    Updated code

--- tests/regression/testCasesPageTests.spec.ts
+++ tests/regression/testCasesPageTests.spec.ts
@@ -1,21 +1,23 @@
 import  test, {Locator, Page, expect} from '@playwright/test';
 import { HomePage } from '../../pages/HomePage';
 import { TestCasesPage } from '../../pages/testCasesPage';
+import { BasePage } from '../../pages/BasePage';
 
 test.describe('End to end test cases', async () => {
     let homePage: HomePage;
     let testCasesPage: TestCasesPage;
+    let basePage: BasePage;
 
     test.beforeEach('Setting up preconditions',async ({page}) => {
         
         homePage = new HomePage(page);
         testCasesPage = new TestCasesPage(page);
-        const url = process.env.baseUrl
-        await page.goto(url!);
+        basePage = new BasePage(page);
+        await page.goto(process.env.baseUrl!);
     });
 
     test('Navigate to Test Cases page', async ({page}) => {
-        await testCasesPage.navigateToTestCasesPage();
+        await basePage.clickOnNavLink('Test Cases');
         await testCasesPage.verifyTestCasesPage();
 
     });
