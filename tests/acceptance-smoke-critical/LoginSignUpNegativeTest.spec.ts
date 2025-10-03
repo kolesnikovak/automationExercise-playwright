6f3788870331c0c9254350623275a321c4450947
kolesnikovak <katyamiller36@gmail.com>
2025-10-02 20:32:58 -0500

    signUp negative test case added

--- tests/acceptance-smoke-critical/LoginSignUpNegativeTest.spec.ts
+++ tests/acceptance-smoke-critical/LoginSignUpNegativeTest.spec.ts
@@ -25,6 +25,15 @@ test.describe('Login test cases', async () => {
         const randomEmail = faker.internet.email();
         const randomPassword = faker.internet.password();
         await loginSignupPage.loginWithEmailAndPassword(randomEmail, randomPassword);
-        await loginSignupPage.validateErrorMessage('Your email or password is incorrect!');
+        await loginSignupPage.validateLoginErrorMessage('Your email or password is incorrect!');
+    });
+    
+      test('SignUp with existing email and verify error message', async ({page}) => {
+        await homePage.validateHomePageTitle();
+        await basePage.clickOnNavLink('Signup / Login');
+        await loginSignupPage.validateSignUpTitle();
+        const existingEmail = process.env.testEmail!;
+        await loginSignupPage.signUpWithNameAndEmail('TestUser', existingEmail);
+        await loginSignupPage.validateSignupErrorMessage('Email Address already exist!');
     });
 });
