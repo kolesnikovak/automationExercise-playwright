f555d2749024656608da7d98897d0aea0495b5e8
kolesnikovak <katyamiller36@gmail.com>
2025-10-04 13:44:24 -0500

    Contact Us test created

---
+++ tests/acceptance-smoke-critical/ContactUsPage.spec.ts
@@ -0,0 +1,28 @@
+import  test, {Locator, Page, expect} from '@playwright/test';
+import { HomePage } from '../../pages/HomePage';
+import { BasePage } from '../../pages/BasePage';
+
+test.describe('Contact Us functionality test cases', async () => {
+    let homePage: HomePage;
+    let basePage: BasePage;
+
+    test.beforeEach('Setting up preconditions',async ({page}) => { 
+        homePage = new HomePage(page);
+        basePage = new BasePage(page);
+        await page.goto(process.env.baseUrl!);
+    });
+    test('Submit contact us form and verify success message', async ({page}) => {
+        await homePage.validateHomePageTitle();
+        await basePage.clickOnNavLink('Contact us');
+        await basePage.verifyContactUsPage();
+        await basePage.fillContactUsForm('Test User', 'test@example.com', 'This is a test message.');
+        await basePage.uploadFileInContactUsForm('/Users/katyakolesnikova/Desktop/MyFirstTypeScriptProject/LearningTypeScript/KnopaTheBestTester.jpg');
+        await page.waitForTimeout(2000);
+        await basePage.submitContactUsForm();
+        await page.waitForTimeout(2000);
+        await basePage.pressOKOnAlert();
+        await basePage.submitContactUsForm();
+        await basePage.verifySuccessMessage();
+        
+    });
+});
